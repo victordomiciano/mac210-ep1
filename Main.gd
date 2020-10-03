8915888ab2adce5f41208cd99f4c5c4d458afb68
Victor Domiciano <victor.dm3@gmail.com>
2020-10-03 16:20:56 -0300

    Add enter, delete and escape input

--- Main.gd
+++ Main.gd
@@ -1,9 +1,9 @@
+# Nome: Victor Domiciano Mendonca
+# NUSP: 8641963
+
 extends Node2D
 
 const BLACK = Color(0, 0, 0)
-const RED = Color(1, 0, 0)
-const GREEN = Color(0, 1, 0)
-const BLUE = Color(0, 0, 1)
 
 var control_points = []
 var drag_points = []
@@ -14,24 +14,63 @@ var last_cubic = false
 var quadratic = []
 var cubic = []
 var first_drag = false
+var end_curve = []
+var idx_first = 0
+var show_drag = false
 
 func _input(event):
 	if event.is_action_pressed('ui_cancel'):
-		get_tree().quit()
+		control_points = []
+		drag_points = []
+		is_dragging = false
+		drag_point = null
+		last_quadratic = false
+		last_cubic = false
+		quadratic = []
+		cubic = []
+		first_drag = false
+		end_curve = []
+		idx_first = 0
+		show_drag = false
+	if event.is_action_pressed('ui_accept'):
+		end_curve.remove(end_curve.size()-1)
+		end_curve.append(true)
+		show_drag = false
+		first_drag = false
+	if event.is_action_pressed('ui_del'):
+		if last_quadratic:
+			control_points.remove(control_points.size()-1)
+			end_curve.remove(end_curve.size()-1)
+			quadratic.remove(quadratic.size()-1)
+			last_quadratic = false
+			drag_points.remove(drag_points.size()-1)
+			drag_points.remove(drag_points.size()-1)
+		control_points.remove(control_points.size()-1)
+		end_curve.remove(end_curve.size()-1)
+		quadratic.remove(quadratic.size()-1)
+		if control_points.size() > 0 and drag_points.size() > 0:
+			if control_points[control_points.size()-1] == drag_points[drag_points.size()-1]:
+				last_quadratic = true
+				quadratic.remove(quadratic.size()-1)
+				control_points.remove(control_points.size()-1)
+				end_curve.remove(end_curve.size()-1)
 	if event.is_action_pressed('left_button'):
 		drag_point = get_viewport().get_mouse_position()
 	if event is InputEventMouseMotion:
 		if drag_point != null and drag_point != get_viewport().get_mouse_position():
 			is_dragging = true
+			show_drag = true
 	if event.is_action_released('left_button'):
 		var mouse_pos = get_viewport().get_mouse_position()
 		if last_quadratic:
 			control_points.append(drag_points[drag_points.size()-1])
+			end_curve.append(false)
 			quadratic.append(false)
 		if last_cubic:
 			cubic.append(false)
 		if not (control_points.size() == 0 and first_drag):
 			control_points.append(drag_point-(mouse_pos-drag_point))
+			end_curve.append(false)
 			quadratic.append(last_quadratic)
 			cubic.append(last_cubic)
 		if is_dragging:
@@ -41,6 +80,7 @@ func _input(event):
 			drag_points.append(drag_point-(mouse_pos-drag_point))
 			drag_points.append(mouse_pos)
 			control_points.append(drag_point)
+			end_curve.append(false)
 			quadratic.append(false)
 			cubic.append(false)
 		else:
@@ -53,22 +93,22 @@ func _input(event):
 func _draw():
 	var drag_size = drag_points.size()
 	var mouse_pos = get_viewport().get_mouse_position()
-	if control_points.size() > 0:
-		var control_size = control_points.size()
-		draw_circle(control_points[0], 3, BLACK)
-		for i in range(1, control_size):
+	var control_size = control_points.size()
+	for i in range(1, control_size):
+		if i > end_curve.size() or (i <= end_curve.size() and not end_curve[i-1]):
 			if not quadratic[i] and not control_points[i-1] in drag_points and not cubic[(i+1)%control_size]:
 				draw_bezier(control_points[i-1], control_points[i], quadratic(control_points[i], i))
-			if cubic[i]:
+			if cubic[i] and not end_curve[i-2]:
 				draw_bezier(control_points[i-2], control_points[i-1], control_points[i], quadratic(control_points[i], i))
-		if drag_size > 1 and not is_dragging:
-			if not (first_drag and drag_size == 2):
+	if control_size > 0 and end_curve.size() == 0 or (end_curve.size() > 0 and not end_curve[end_curve.size()-1]):
+		if drag_size > 1 and not is_dragging and show_drag:
+			if not first_drag or drag_size > 2:
 				draw_circle(drag_points[drag_size-2], 3, BLACK)
 				draw_circle(drag_points[drag_size-1], 3, BLACK)
 				draw_line(drag_points[drag_size-2], drag_points[drag_size-1], BLACK)
 			else:
-				draw_circle(drag_points[1], 3, BLACK)
-				draw_line(control_points[0], drag_points[1], BLACK)
+				draw_circle(drag_points[drag_size-1], 3, BLACK)
+				draw_line(control_points[idx_first], drag_points[drag_size-1], BLACK)
 		if is_dragging:
 			draw_circle(mouse_pos, 3, BLACK)
 			draw_circle(drag_point-(mouse_pos-drag_point), 3, BLACK)
@@ -82,10 +122,14 @@ func _draw():
 				draw_bezier(control_points[control_size-1], drag_points[drag_size-1], mouse_pos)
 			else:
 				draw_line(control_points[control_size-1], mouse_pos, BLACK)
-	elif is_dragging:
-		draw_circle(drag_point, 3, BLACK)
+		return
+	if is_dragging:
 		draw_line(drag_point, mouse_pos, BLACK)
 		first_drag = true
+		if end_curve.size() > 0:
+			idx_first = end_curve.size()+1
+		else:
+			idx_first = 0
 
 func quadratic(p, i):
 	if i < control_points.size()-1 and drag_points.has(p):
@@ -103,6 +147,9 @@ func draw_bezier(p0, p1, p2=null, p3=null):
 	if p3 == null:
 		draw_quadratic_bezier(p0, p1, p2)
 		return
+	draw_cubic_bezier(p0, p1, p2, p3)
+
+func draw_cubic_bezier(p0, p1, p2, p3):
 	var weight = 0.0
 	var r0 = p0
 	var q0
@@ -110,7 +157,7 @@ func draw_bezier(p0, p1, p2=null, p3=null):
 	var q2
 	var r1
 	while weight < 1.0:
-		weight += 0.01
+		weight += 0.05
 		q0 = lerp_vector(p0, p1, weight)
 		q1 = lerp_vector(p1, p2, weight)
 		q2 = lerp_vector(p2, p3, weight)
@@ -123,7 +170,7 @@ func draw_quadratic_bezier(p0, p1, p2):
 	var q0 = p0
 	var q1
 	while weight < 1.0:
-		weight += 0.01
+		weight += 0.05
 		q1 = lerp_vector(lerp_vector(p0, p1, weight), lerp_vector(p1, p2, weight), weight)
 		draw_line(q0, q1, BLACK)
 		q0 = q1
