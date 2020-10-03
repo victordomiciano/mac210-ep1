d6739b9b3e9f3d0d59751efc382a7dca0edb4620
Victor Domiciano <victor.dm3@gmail.com>
2020-10-03 16:19:34 -0300

    Add cubic bezier curves

--- Main.gd
+++ Main.gd
@@ -10,7 +10,9 @@ var drag_points = []
 var is_dragging = false
 var drag_point = null
 var last_quadratic = false
+var last_cubic = false
 var quadratic = []
+var cubic = []
 var first_drag = false
 
 func _input(event):
@@ -26,17 +28,24 @@ func _input(event):
 		if last_quadratic:
 			control_points.append(drag_points[drag_points.size()-1])
 			quadratic.append(false)
+		if last_cubic:
+			cubic.append(false)
 		if not (control_points.size() == 0 and first_drag):
 			control_points.append(drag_point-(mouse_pos-drag_point))
 			quadratic.append(last_quadratic)
+			cubic.append(last_cubic)
 		if is_dragging:
 			last_quadratic = true
+			if last_quadratic:
+				last_cubic = true
 			drag_points.append(drag_point-(mouse_pos-drag_point))
 			drag_points.append(mouse_pos)
 			control_points.append(drag_point)
 			quadratic.append(false)
+			cubic.append(false)
 		else:
 			last_quadratic = false
+			last_cubic = false
 		is_dragging = false
 		drag_point = null
 	update()
@@ -48,8 +57,10 @@ func _draw():
 		var control_size = control_points.size()
 		draw_circle(control_points[0], 3, BLACK)
 		for i in range(1, control_size):
-			if not quadratic[i] and not control_points[i-1] in drag_points:
+			if not quadratic[i] and not control_points[i-1] in drag_points and not cubic[(i+1)%control_size]:
 				draw_bezier(control_points[i-1], control_points[i], quadratic(control_points[i], i))
+			if cubic[i]:
+				draw_bezier(control_points[i-2], control_points[i-1], control_points[i], quadratic(control_points[i], i))
 		if drag_size > 1 and not is_dragging:
 			if not (first_drag and drag_size == 2):
 				draw_circle(drag_points[drag_size-2], 3, BLACK)
@@ -61,8 +72,11 @@ func _draw():
 		if is_dragging:
 			draw_circle(mouse_pos, 3, BLACK)
 			draw_circle(drag_point-(mouse_pos-drag_point), 3, BLACK)
-			draw_bezier(control_points[control_size-1], drag_point-(mouse_pos-drag_point), drag_point)
 			draw_line(drag_point-(mouse_pos-drag_point), mouse_pos, BLACK)
+			if last_quadratic:
+				draw_bezier(control_points[control_size-1], drag_points[drag_size-1], drag_point-(mouse_pos-drag_point), drag_point)
+			else:
+				draw_bezier(control_points[control_size-1], drag_point-(mouse_pos-drag_point), drag_point)
 		else:
 			if last_quadratic:
 				draw_bezier(control_points[control_size-1], drag_points[drag_size-1], mouse_pos)
@@ -82,10 +96,29 @@ func quadratic(p, i):
 func lerp_vector(p, q, weight):
 	return Vector2(lerp(p.x, q.x, weight), lerp(p.y, q.y, weight))
 
-func draw_bezier(p0, p1, p2=null):
+func draw_bezier(p0, p1, p2=null, p3=null):
 	if p2 == null:
 		draw_line(p0, p1, BLACK)
 		return
+	if p3 == null:
+		draw_quadratic_bezier(p0, p1, p2)
+		return
+	var weight = 0.0
+	var r0 = p0
+	var q0
+	var q1
+	var q2
+	var r1
+	while weight < 1.0:
+		weight += 0.01
+		q0 = lerp_vector(p0, p1, weight)
+		q1 = lerp_vector(p1, p2, weight)
+		q2 = lerp_vector(p2, p3, weight)
+		r1 = lerp_vector(lerp_vector(q0, q1, weight), lerp_vector(q1, q2, weight), weight)
+		draw_line(r0, r1, BLACK)
+		r0 = r1
+
+func draw_quadratic_bezier(p0, p1, p2):
 	var weight = 0.0
 	var q0 = p0
 	var q1
